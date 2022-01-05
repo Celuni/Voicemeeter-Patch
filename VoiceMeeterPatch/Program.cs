45462e8f14adc97e17923294937a9cef4db86de9
skidword64 <74870446+dword64@users.noreply.github.com>
2022-01-05 16:06:13 +0100

    added x32 and killProcess

--- VoiceMeeterPatch/Program.cs
+++ VoiceMeeterPatch/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+using System;
+using System.Diagnostics;
+
 
 namespace VoiceMeeterPatch
 {
@@ -6,17 +8,65 @@ namespace VoiceMeeterPatch
     {
         static void Main(string[] args)
         {
-            patchVoiceMeterBytes(@"C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8x64.exe");
+            killProcess("voicemeeter8x64");
+            killProcess("voicemeeter8");
 
-            Console.WriteLine("Voicemeeter cracked! press any key to leave");
+            patchVoiceMeterBytesX64(@"C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8x64.exe");
+            patchVoiceMeterBytesX32(@"C:\Program Files (x86)\VB\Voicemeeter\voicemeeter8.exe");
+
+            Console.WriteLine("Voicemeeter Patched! You can start Voicemeeter now! >>> press any key to leave");
             Console.ReadLine();
         }
 
-        public static void patchVoiceMeterBytes(string path)
+        public static void killProcess(string name)
         {
-            if (System.IO.File.Exists(path) == false)
+            foreach (Process process in Process.GetProcessesByName(name))
+            {
+                if (process == null || process.MainWindowHandle == IntPtr.Zero)
+                    continue;
+
+                try
+                {
+                    process.Kill();
+                    process.WaitForExit(5000);
+                }
+                catch { }
+            }
+        }
+
+        public static void patchVoiceMeterBytesX32(string pathx32)
+        {
+            if (System.IO.File.Exists(pathx32) == false)
                 return;
-            byte[] fileBytes = System.IO.File.ReadAllBytes(path);
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(pathx32);
+            fileBytes[0x8687C] = 0xC7;
+
+            fileBytes[0x86882] = 0x13;
+            fileBytes[0x86883] = 0x0;
+            fileBytes[0x86884] = 0x0;
+            fileBytes[0x86885] = 0x0;
+            fileBytes[0x86886] = 0x0;
+            fileBytes[0x86887] = 0x90;
+            fileBytes[0x86888] = 0x90;
+
+            fileBytes[0xA67E3] = 0xC7;
+
+            fileBytes[0xA67E9] = 0x13;
+            fileBytes[0xA67EA] = 0x0;
+            fileBytes[0xA67EB] = 0x0;
+            fileBytes[0xA67EC] = 0x0;
+
+
+            System.IO.File.WriteAllBytes(pathx32, fileBytes);
+        }
+
+        public static void patchVoiceMeterBytesX64(string pathx64)
+        {
+            if (System.IO.File.Exists(pathx64) == false)
+                return;
+
+            byte[] fileBytes = System.IO.File.ReadAllBytes(pathx64);
             fileBytes[0x93880] = 0xC7;
             fileBytes[0xB5BCB] = 0xC7;
 
@@ -31,7 +81,7 @@ namespace VoiceMeeterPatch
             fileBytes[0xB5BD4] = 0x0;
             fileBytes[0xB5BD5] = 0x90;
 
-            System.IO.File.WriteAllBytes(path, fileBytes);
+            System.IO.File.WriteAllBytes(pathx64, fileBytes);
         }
     }
 }
